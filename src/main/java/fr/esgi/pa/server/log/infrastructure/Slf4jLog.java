705e0ae109e7681c87d3e20db0fc43d0ce9d0a5d
MasatakaIshii <masaradio78@gmail.com>
2021-05-04 01:29:08 +0200

    Change name of log implementation to slf4j

--- src/main/java/fr/esgi/pa/server/log/infrastructure/Slf4jLog.java
+++ src/main/java/fr/esgi/pa/server/log/infrastructure/Slf4jLog.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LogImpl<T> implements Log<T> {
+public class Slf4jLog<T> implements Log<T> {
 
     @Override
     public void info(Class<T> aClass, String message) {
