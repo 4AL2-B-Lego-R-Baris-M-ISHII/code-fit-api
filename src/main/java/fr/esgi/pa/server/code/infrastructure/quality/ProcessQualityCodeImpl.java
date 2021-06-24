854346bfc41391872950dbfdab70e39bf020756d
MasatakaIshii <masaradio78@gmail.com>
2021-06-25 00:56:47 +0200

    ProcessQualityCode done

--- src/main/java/fr/esgi/pa/server/code/infrastructure/quality/ProcessQualityCodeImpl.java
+++ src/main/java/fr/esgi/pa/server/code/infrastructure/quality/ProcessQualityCodeImpl.java
@@ -4,14 +4,25 @@ import fr.esgi.pa.server.code.core.dto.CodeQualityType;
 import fr.esgi.pa.server.code.core.quality.ProcessQualityCode;
 import fr.esgi.pa.server.code.core.quality.QualityCode;
 import fr.esgi.pa.server.language.core.Language;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
 @Component
+@RequiredArgsConstructor
 public class ProcessQualityCodeImpl implements ProcessQualityCode {
+    private final QualityCodeActionFactory factory;
+
     @Override
-    public QualityCode process(String content, Language language, Set<CodeQualityType> qualityTypeStack) {
-        return null;
+    public QualityCode process(String content, Language language, Set<CodeQualityType> codeQualityTypeSet) {
+        var qualityCode = new QualityCode()
+                .setCodeContent(content)
+                .setLanguage(language);
+        for (CodeQualityType type : codeQualityTypeSet) {
+            var action = factory.getAction(type);
+            qualityCode = action.execute(qualityCode);
+        }
+        return qualityCode;
     }
 }
--- src/test/java/fr/esgi/pa/server/unit/code/infrastructure/quality/ProcessQualityCodeImplTest.java
+++ src/test/java/fr/esgi/pa/server/unit/code/infrastructure/quality/ProcessQualityCodeImplTest.java
@@ -1,15 +1,22 @@
 package fr.esgi.pa.server.unit.code.infrastructure.quality;
 
 import fr.esgi.pa.server.code.core.dto.CodeQualityType;
+import fr.esgi.pa.server.code.core.quality.QualityCode;
 import fr.esgi.pa.server.code.infrastructure.quality.ProcessQualityCodeImpl;
+import fr.esgi.pa.server.code.infrastructure.quality.QualityCodeActionFactory;
+import fr.esgi.pa.server.code.infrastructure.quality.action.QualityCodeAction;
 import fr.esgi.pa.server.language.core.Language;
 import fr.esgi.pa.server.language.core.LanguageName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProcessQualityCodeImplTest {
@@ -23,16 +30,46 @@ class ProcessQualityCodeImplTest {
 
     private ProcessQualityCodeImpl sut;
 
+    @Mock
+    private QualityCodeActionFactory mockQualityCodeActionFactory;
+
+    @Mock
+    private QualityCodeAction mockQualityCodeAction;
+
     @BeforeEach
     void setup() {
-        sut = new ProcessQualityCodeImpl();
+        sut = new ProcessQualityCodeImpl(mockQualityCodeActionFactory);
 
     }
 
     @Test
-    void when_qualityTypeStack_contain_one_code_quality_type_should_call_factory_to_get_concerned_action_code_quality() {
+    void when_get_action_by_factory_should_call_action_command() {
         codeQualityTypeSet = Set.of(CodeQualityType.LINES_CODE);
+        when(mockQualityCodeActionFactory.getAction(CodeQualityType.LINES_CODE)).thenReturn(mockQualityCodeAction);
 
         sut.process(codeContent, language, codeQualityTypeSet);
+
+        var expectedQualityCode = new QualityCode()
+                .setCodeContent(codeContent)
+                .setLanguage(language);
+        verify(mockQualityCodeAction, times(1)).execute(expectedQualityCode);
+    }
+
+    @Test
+    void when_only_one_set_of_code_quality_type_should_update_one_times_and_return_quality_code() {
+        codeQualityTypeSet = Set.of(CodeQualityType.LINES_CODE);
+        when(mockQualityCodeActionFactory.getAction(CodeQualityType.LINES_CODE)).thenReturn(mockQualityCodeAction);
+        var qualityCode = new QualityCode()
+                .setCodeContent(codeContent)
+                .setLanguage(language);
+        var updatedQualityCode = new QualityCode()
+                .setCodeContent(codeContent)
+                .setLanguage(language)
+                .setLineCode(10L);
+        when(mockQualityCodeAction.execute(qualityCode)).thenReturn(updatedQualityCode);
+
+        var result = sut.process(codeContent, language, codeQualityTypeSet);
+
+        assertThat(result).isEqualTo(updatedQualityCode);
     }
 }
