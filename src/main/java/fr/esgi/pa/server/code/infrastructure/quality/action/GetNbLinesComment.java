699b64811620fc33e3053f540dbe98cf21fb7dd6
MasatakaIshii <masaradio78@gmail.com>
2021-06-27 18:20:48 +0200

    QualityCodeActionFactory to create GetNbLinesComment done

---
+++ src/main/java/fr/esgi/pa/server/code/infrastructure/quality/action/GetNbLinesComment.java
@@ -0,0 +1,13 @@
+package fr.esgi.pa.server.code.infrastructure.quality.action;
+
+import fr.esgi.pa.server.code.core.quality.QualityCode;
+import fr.esgi.pa.server.code.infrastructure.quality.action.by_language.ActionsByLanguage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetNbLinesComment implements QualityCodeAction {
+    @Override
+    public QualityCode execute(ActionsByLanguage actionsByLanguage, QualityCode currentQualityCode) {
+        return null;
+    }
+}
