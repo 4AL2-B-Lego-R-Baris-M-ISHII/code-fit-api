5863e7e504e510a9440d0c966dc6f76936904900
MasatakaIshii <masaradio78@gmail.com>
2021-06-27 18:31:10 +0200

    Prepare SlashesNbLinesComment

---
+++ src/main/java/fr/esgi/pa/server/code/infrastructure/quality/action/by_language/nb_lines_comment/SlashesNbLinesComment.java
@@ -0,0 +1,11 @@
+package fr.esgi.pa.server.code.infrastructure.quality.action.by_language.nb_lines_comment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SlashesNbLinesComment implements GetNbLinesCommentByLanguage {
+    @Override
+    public Long execute(String content) {
+        return null;
+    }
+}
---
+++ src/test/java/fr/esgi/pa/server/unit/code/infrastructure/quality/action/by_language/nb_lines_comment/SlashesNbLinesCommentTest.java
@@ -0,0 +1,5 @@
+package fr.esgi.pa.server.unit.code.infrastructure.quality.action.by_language.nb_lines_comment;
+
+class SlashesNbLinesCommentTest {
+
+}
