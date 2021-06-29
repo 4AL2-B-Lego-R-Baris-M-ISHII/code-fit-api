1004a85799d1ffb2a8650cc64c8fe640dd026908
MasatakaIshii <masaradio78@gmail.com>
2021-06-29 10:28:37 +0200

    SlashesNbLinesComment execute code improve

--- src/main/java/fr/esgi/pa/server/code/infrastructure/quality/action/by_language/nb_lines_comment/SlashesNbLinesComment.java
+++ src/main/java/fr/esgi/pa/server/code/infrastructure/quality/action/by_language/nb_lines_comment/SlashesNbLinesComment.java
@@ -4,94 +4,41 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SlashesNbLinesComment implements GetNbLinesCommentByLanguage {
-//    private final Map<Function<String, Boolean>, Consumer<String>> mapAction;
-//    private ResultNbLinesComment resultNbLinesComment;
-
-//    public SlashesNbLinesComment() {
-//        mapAction = new HashMap<>();
-//        mapAction.put(this::isLineContainStartsOfMultilineCommentAndSimpleComment, (line) -> {
-//            if (line.indexOf("/*") < line.indexOf("//")) {
-//                resultNbLinesComment.finishMultiLinesCommentIfEnd(line);
-//            }
-//            resultNbLinesComment.incrementResult();
-//        });
-//        mapAction.put(this::isLineContainMultilineCommentStartOrEnd, (line) -> {
-//            resultNbLinesComment.finishMultiLinesCommentIfEnd(line);
-//            resultNbLinesComment.incrementResult();
-//        });
-//        mapAction.put(this::isLineContainStartSimpleComment, (line) -> {
-//            resultNbLinesComment.incrementResult();
-//        });
-//    }
 
     @Override
     public Long execute(String content) {
-//        var linesContent = content.split("\\r\\n|\\r|\\n");
-//        resultNbLinesComment = new ResultNbLinesComment();
-//        for (String line : linesContent) {
-//            this.actionDependToLineContent(line, resultNbLinesComment);
-//        }
-//        return resultNbLinesComment.getResult();
         var linesContent = content.split("\\r\\n|\\r|\\n");
-        //var resultNbLinesComment = new ResultNbLinesComment();
-        var result = 0L;
-        var isInMultiLinesComments = false;
+        var resultNbLinesComment = new ResultNbLinesComment();
         for (String line : linesContent) {
-            if (line.contains("//") && line.contains("/*")) {
-                if (line.indexOf("/*") < line.indexOf("//")) {
-                    isInMultiLinesComments = !line.contains("*/");
-                }
-                result++;
-            } else if (line.contains("//")) {
-                result++;
-            } else if (line.contains("/*")) {
-                result++;
-                isInMultiLinesComments = !line.contains("*/");
-            } else if (line.contains("*/")) {
-                isInMultiLinesComments = false;
-                result++;
-            } else if (isInMultiLinesComments) {
-                result++;
-            }
+            setResultDependToLine(resultNbLinesComment, line);
         }
-        return result;
+        return resultNbLinesComment.getResult();
     }
 
-    //    private void actionDependToLineContent(String line, ResultNbLinesComment resultNbLinesComment) {
-//        var maybeActionResult = getOneActionDependToLineContent(line);
-//        var actionOnResult = maybeActionResult.map(Map.Entry::getValue).orElse((map) -> {
-//            if (resultNbLinesComment.isInMultiLinesComments) {
-//                resultNbLinesComment.incrementResult();
-//            }
-//        });
-//        actionOnResult.accept(line);
-//    }
-//
-//    @NotNull
-//    private Optional<Map.Entry<Function<String, Boolean>, Consumer<String>>> getOneActionDependToLineContent(String line) {
-//        return mapAction.entrySet().stream()
-//                .filter((verifyLine) -> verifyLine.getKey().apply(line))
-//                .findFirst();
-//    }
-//
-//    private boolean isLineContainStartsOfMultilineCommentAndSimpleComment(String line) {
-//        return line.contains("//") && line.contains("/*");
-//    }
-//
-//    private boolean isLineContainMultilineCommentStartOrEnd(String line) {
-//        return line.contains("/*") || line.contains("*/");
-//    }
-//
-//    private boolean isLineContainStartSimpleComment(String line) {
-//        return line.contains("//");
-//    }
-//
-    private class ResultNbLinesComment {
+    private void setResultDependToLine(ResultNbLinesComment resultNbLinesComment, String line) {
+        if (line.contains("//") && line.contains("/*")) {
+            if (line.indexOf("/*") < line.indexOf("//")) {
+                resultNbLinesComment.finishMultiLinesCommentIfEnd(line);
+            }
+            resultNbLinesComment.incrementResult();
+        } else if (line.contains("/*") || line.contains("*/")) {
+            resultNbLinesComment.incrementResult();
+            resultNbLinesComment.finishMultiLinesCommentIfEnd(line);
+        } else if (line.contains("//") || resultNbLinesComment.getInMultiLinesComments()) {
+            resultNbLinesComment.incrementResult();
+        }
+    }
+
+    private static class ResultNbLinesComment {
         private Long result = 0L;
         private Boolean isInMultiLinesComments = false;
 
         public Long getResult() {
             return result;
+        }
+
+        public Boolean getInMultiLinesComments() {
+            return isInMultiLinesComments;
         }
 
         public void finishMultiLinesCommentIfEnd(String line) {
