534e3487466f2931787f0ba6dea4b6df7d4f35f3
MasatakaIshii <masaradio78@gmail.com>
2021-07-04 23:13:47 +0200

    refacto ActionsByJava getCyclomaticComplexity done

--- src/main/java/fr/esgi/pa/server/code/infrastructure/quality/action/by_language/ActionsByJava.java
+++ src/main/java/fr/esgi/pa/server/code/infrastructure/quality/action/by_language/ActionsByJava.java
@@ -1,14 +1,10 @@
 package fr.esgi.pa.server.code.infrastructure.quality.action.by_language;
 
-import fr.esgi.pa.server.code.infrastructure.gen.java8.Java8Lexer;
-import fr.esgi.pa.server.code.infrastructure.gen.java8.Java8Parser;
+import fr.esgi.pa.server.code.infrastructure.quality.action.by_language.cyclomatic_complexity.GetCyclomaticComplexity;
 import fr.esgi.pa.server.code.infrastructure.quality.action.by_language.nb_lines_code.GetNbLinesCodeByLanguage;
 import fr.esgi.pa.server.code.infrastructure.quality.action.by_language.nb_lines_comment.GetNbLinesCommentByLanguage;
+import fr.esgi.pa.server.language.core.LanguageName;
 import lombok.RequiredArgsConstructor;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.springframework.stereotype.Component;
 
 import java.util.Hashtable;
@@ -20,6 +16,7 @@ import java.util.Map;
 public class ActionsByJava implements ActionsByLanguage {
     private final GetNbLinesCodeByLanguage getNbLinesCodeByLanguage;
     private final GetNbLinesCommentByLanguage getNbLinesCommentByLanguage;
+    private final GetCyclomaticComplexity getCyclomaticComplexity;
 
     @Override
     public Long getNbLinesCode(String content) {
@@ -33,11 +30,6 @@ public class ActionsByJava implements ActionsByLanguage {
 
     @Override
     public Long getCyclomaticComplexity(String content) {
-        Java8Lexer lexer = new Java8Lexer(CharStreams.fromString(content));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        Java8Parser parser = new Java8Parser(tokens);
-        ParseTree tree = parser.compilationUnit();
-        Long result = 0L;
         var listNodeTextCorrespondCycloComplex = List.of(
                 "if", "case", "for", "while", "forEach"
         );
@@ -46,21 +38,6 @@ public class ActionsByJava implements ActionsByLanguage {
             mapNodeCorrespondCycloComplex.put(nodeText, true);
         });
 
-        return searchNodeCorrespondCyclomaticComplexity(tree, parser, result, mapNodeCorrespondCycloComplex);
-    }
-
-    private Long searchNodeCorrespondCyclomaticComplexity(ParseTree currentTree, Parser parser, Long result, Map<String, Boolean> mapNodeCorrespondCycloComplex) {
-//        System.out.printf("toStringtree : %s%n", currentTree.toStringTree(parser));
-//        System.out.printf("getText : %s%n", currentTree.getText());
-//        System.out.printf("getClass : %s%n", currentTree.getClass());
-//        System.out.println();
-        for (int i = 0; i < currentTree.getChildCount(); i++) {
-            result = searchNodeCorrespondCyclomaticComplexity(currentTree.getChild(i), parser, result, mapNodeCorrespondCycloComplex);
-        }
-        var textNode = currentTree.getText();
-        if (!textNode.trim().isBlank() && mapNodeCorrespondCycloComplex.containsKey(textNode)) {
-            result++;
-        }
-        return result;
+        return getCyclomaticComplexity.execute(LanguageName.JAVA8, mapNodeCorrespondCycloComplex, content);
     }
 }
