8f1ea57721ab50bcb144ca70e883a93dbe56165f
MasatakaIshii <masaradio78@gmail.com>
2021-07-24 19:11:23 +0200

    Prepare method findByUserIdAndExerciseCaseId of CodeDao

--- src/test/java/fr/esgi/pa/server/unit/exercise/usecase/AddLoggedUserCodeAllExercisesTest.java
+++ src/test/java/fr/esgi/pa/server/unit/exercise/usecase/AddLoggedUserCodeAllExercisesTest.java
@@ -1,9 +1,22 @@
 package fr.esgi.pa.server.unit.exercise.usecase;
 
+import fr.esgi.pa.server.exercise.usecase.AddLoggedUserCodeAllExercises;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class AddLoggedUserCodeAllExercisesTest {
+    private AddLoggedUserCodeAllExercises sut;
 
+    @BeforeEach
+    void setup() {
+        sut = new AddLoggedUserCodeAllExercises();
+    }
+
+    @Test
+    void when_setDtoExercise_contain_one_dto_exercise_with_one_exercise_case_should_call() {
+
+    }
 }
